237f7a06a64a2b66464e8ac3374c1e153bbbe50f
Lucaslimasz <id.lucas@outlook.com.br>
2022-03-06 11:50:38 -0300

    returning treated values

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { GetStaticProps } from "next";
-import api from "../config/api";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-interface EpisodesProps {
+import api from "../config/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+interface Episodes {
   id: string;
   title: string;
   members: string;
@@ -16,18 +20,42 @@ interface EpisodesProps {
 }
 
 interface HomeProps {
-  episodes: EpisodesProps[];
+  episodes: Episodes[];
 }
 
 export default function Home({ episodes }: HomeProps) {
-  return <div></div>;
+  return <div>{JSON.stringify(episodes)}</div>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get("/episodes");
+  const { data } = await api.get("/episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode: any) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.id,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      duration: Number(episode.file.duration),
+      url: episode.file.url,
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes: episodes,
     },
     revalidate: 60 * 60 * 8, // 8 hours
   };
